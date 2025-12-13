87e88f0281f9a59b6ee936e70387503fc54d3b81
KailashPathak <cypress1980@example.com>
2025-12-13 12:32:52 +0530

    allure report integration

--- playwright.config.js
+++ playwright.config.js
@@ -24,8 +24,22 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI 
-    ? [['blob'], ['allure-playwright', { outputFolder: 'allure-results' }]]
-    : [['html'], ['allure-playwright', { outputFolder: 'allure-results' }]],
+    ? [
+        ['blob'],
+        ['allure-playwright', { 
+          outputFolder: 'allure-results',
+          detail: true,
+          suiteTitle: false
+        }]
+      ]
+    : [
+        ['html'],
+        ['allure-playwright', { 
+          outputFolder: 'allure-results',
+          detail: true,
+          suiteTitle: false
+        }]
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
